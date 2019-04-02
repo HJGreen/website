47710f0123cde37e2a0ae8d02f214c2fd6669b10
Harry Green <harry@hgreen.co.uk>
2019-04-02 14:14:16 +1300

    Update site meta information

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,18 +9,18 @@ import "./../layouts/index.css";
 const TemplateWrapper = () => (
   <div>
     <Helmet
-      title="Harry Green - Full Stack Web Developer"
+      title="Harry Green - Full Stack Developer"
       meta={[
         {
           name: "description",
-          content: "Harry Green - Full stack web developer."
+          content: "Full Stack developer specialising in WordPress and React. Currently based in Wellington, New Zealand"
         },
         {
           name: "keywords",
           content:
-            "web developer, full stack, php, javascript, react, front end, website"
+            "web developer, full stack, php, javascript, react, front end, website, wordpress, wellington, new zealand, svg, portfolio"
         },
-        { name: "theme-color", content: "#ebedef" },
+        { name: "theme-color", content: "#1a202c" },
         { name: "viewport", content: "width=device-width, initial-scale=1" }
       ]}
     >
